176cad45e178c678f4d803764c51a6bd2a3e47a2
Soloma <soloma@tinaj.ru>
2024-03-24 15:41:53 +0500

    mode in config

--- config/config.go
+++ config/config.go
@@ -15,7 +15,7 @@ type Config struct {
 }
 
 type HttpServer struct {
-	Address     string        `yaml:"address" env-default:"localhost:1436"`
+	Address     string        `yaml:"address" env-default:"0.0.0.0:1436"`
 	Timeout     time.Duration `yaml:"timeout" env-default:"4s"`
 	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
 }
